40f90caec0e25c1164f0ae80abb38cfde24e7df7
Healer928802 <78316893+Healer928802@users.noreply.github.com>
2025-01-25 12:05:06 +0200

    Prepare Project Structure

---
+++ CurrencyExchange/CurrencyExchange/Helpers/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  CurrencyExchange
+//
+//  Created by Anton on 25.01.2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
